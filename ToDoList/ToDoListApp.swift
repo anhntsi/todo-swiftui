3ac7723dea730c1ccc6bb116a0a6d79d263395e2
Anh Nguyen <anhnt.si@gmail.com>
2023-05-24 21:57:08 +0700

    create project

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by The Anh Nguyen on 24/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
